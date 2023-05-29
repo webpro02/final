b5defefd43c1aa318833922e110f101d3fe63f1c
김세윤 <@host.docker.internal>
2023-05-29 23:52:35 +0900

    로그인+길찾기+게시판 합침 | lombok 오류 발생

---
+++ WebProgrammingTeamProject/src/main/java/com/example/demo/BoardService.java
@@ -0,0 +1,59 @@
+package com.example.demo;
+
+import com.example.demo.Board;
+import com.example.demo.BoardRepository;
+import com.example.demo.BoardDto;
+import org.springframework.stereotype.Service;
+
+import jakarta.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class BoardService {
+    private BoardRepository boardRepository;
+
+    public BoardService(BoardRepository boardRepository) {
+        this.boardRepository = boardRepository;
+    }
+
+    @Transactional
+    public Long savePost(BoardDto boardDto) {
+        return boardRepository.save(boardDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public List<BoardDto> getBoardList() {
+        List<Board> boardList = boardRepository.findAll();
+        List<BoardDto> boardDtoList = new ArrayList<>();
+
+        for(Board board : boardList) {
+            BoardDto boardDto = BoardDto.builder()
+                    .id(board.getId())
+                    .author(board.getAuthor())
+                    .title(board.getTitle())
+                    .content(board.getContent())
+                    .createdDate(board.getCreatedDate())
+                    .build();
+            boardDtoList.add(boardDto);
+        }
+        return boardDtoList;
+    }
+    @Transactional
+    public BoardDto getPost(Long id) {
+        Board board = boardRepository.findById(id).get();
+
+        BoardDto boardDto = BoardDto.builder()
+                .id(board.getId())
+                .author(board.getAuthor())
+                .title(board.getTitle())
+                .content(board.getContent())
+                .createdDate(board.getCreatedDate())
+                .build();
+        return boardDto;
+    }
+    @Transactional
+    public void deletePost(Long id) {
+        boardRepository.deleteById(id);
+    }
+}
